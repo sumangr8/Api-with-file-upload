db3d8c72cae811516449f96770766b4e60f549e5
sumangr8 <30518152+sumangr8@users.noreply.github.com>
2021-08-01 12:38:29 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,18 +1,26 @@
 <?php
-header('Content-Type:application/json');
-// header('Access-Control-Allow-Origin : *');
-include("db.php");
+
+header("Content-Type: application/json");
+// header("Acess-Control-Allow-Origin: *");
+// header("Acess-Control-Allow-Methods: POST");
+// header("Acess-Control-Allow-Headers: Acess-Control-Allow-Headers,Content-Type,Acess-Control-Allow-Methods, Authorization");
+
+include "db.php";
 $data=json_decode(file_get_contents("php://input"),true);
 $name=$data["name"];
 $email=$data["email"];
 $password=$data["password"];
-$qry=mysqli_query($con,"insert into login (name,email,password) values ('$name','$email','$password')");
-if($qry)
+$mobile=$data["mobile"];
+$qualification=implode(",", $data["qualification"]);
+$sql="insert into login (name,email,password,mobile,qualification) values('$name','$email','$password','$mobile','$qualification')";
+$result=$con->query($sql);
+if($result==1)
 {
-	echo json_encode(array("message"=>"Insert success","status"=>true));
+	echo json_encode(array("status"=>1,"message"=>"Insert Success"));
 }
 else
 {
-	echo json_encode(array("message"=>"Insert faield","status"=>false));
+	echo json_encode(array("status"=>0,"message"=>"Insert Faield"));
 }
 ?>
+
